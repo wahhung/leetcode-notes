ba15b0230e2fb9561a657fe71a2067ca89aba7fd
Wahhung <sunhuaxiong@126.com>
2021-04-15 00:43:56 +0800

    Update findKthLargest.go

--- 2021-03-18/code215/findKthLargest.go
+++ 2021-03-18/code215/findKthLargest.go
@@ -15,37 +15,36 @@ func main() {
 // 堆是一棵完全二叉树，
 // 最大堆：根节点的键值是所有堆结点键值中最大值，且每个结点的值都比其孩子的值大
 // 最小堆：根结点的键值是所有堆结点键值中最小值，且每个结点的值都比其孩子的值小
+// 解题思路：利用堆排序，进排序后面的 n-k个，即剩下的为k最大堆
 // Time:O(nlogn)
 // Space:O(logn)
 func findKthLargest(nums []int, k int) int {
-	heapSize := len(nums)
-	buildMaxHeap(nums, heapSize)
-	for i := len(nums) - 1; i >= len(nums)-k+1; i-- {
+    // 父子结点的关系，只需要遍历一半即可,i~len(nums)
+	for i := len(nums)/2 - 1; i >= 0; i-- {
+		adjust(nums, i, len(nums))
+	}
+	for i := len(nums) - 1; i > len(nums) - k; i-- {
 		nums[0], nums[i] = nums[i], nums[0]
-		heapSize--
-		maxHeapify(nums, 0, heapSize)
+		adjust(nums, 0, i)
 	}
-	return nums[0]
+    return nums[0]
 }
 
-func buildMaxHeap(a []int, heapSize int) {
-	for i := heapSize / 2; i >= 0; i-- {
-		maxHeapify(a, i, heapSize)
+// 最大堆
+func adjust(nums []int, start, end int) {
+	target := nums[start]
+	for i := start * 2 + 1; i < end; i = i * 2 + 1 {
+		if i + 1 < end && nums[i] < nums[i+1] {
+			i++
+		}
+		if target < nums[i] {
+			nums[start] = nums[i]
+			start = i
+		} else {
+			break
+		}
 	}
-}
-
-func maxHeapify(a []int, i, heapSize int) {
-	l, r, largest := i*2+1, i*2+2, i
-	if l < heapSize && a[l] > a[largest] {
-		largest = l
-	}
-	if r < heapSize && a[r] > a[largest] {
-		largest = r
-	}
-	if largest != i {
-		a[i], a[largest] = a[largest], a[i]
-		maxHeapify(a, largest, heapSize)
-	}
+	nums[start] = target
 }
 
 // 基于快速排序的选择方法
